34b4b96993118cae6636dd9a5c3e183ece482687
Thálisson Sinézio <tms.working@gmail.com>
2022-02-23 23:07:42 -0300

    feat(User): add create user with SOLID

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,18 +1,18 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  id?:string,
-  name:string,
-  email:string,
-  admin:boolean,
-  created_at:string,
-  updated_at: string,
-  
-  constructor(){
-    if(!this.id){
+  id?: string;
+  name: string;
+  email: string;
+  admin: boolean;
+  created_at: string;
+  updated_at: string;
+
+  constructor() {
+    if (!this.id) {
       this.id = uuidV4();
     }
-    if(!this.admin){
+    if (!this.admin) {
       this.admin = false;
     }
   }
